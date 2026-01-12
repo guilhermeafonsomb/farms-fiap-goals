1f10a2331175ba6fe329ff4350900ceaaedf4571
Guilherme Afonso <guilhermeafonsomb@gmail.com>
2026-01-12 15:57:06 -0300

    chore: setup msw

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,7 @@
 import "@testing-library/jest-dom/vitest";
+import { server } from "@/lib/mocks/server";
+import { beforeAll, afterEach, afterAll } from "vitest";
+
+beforeAll(() => server.listen({ onUnhandledRequest: "warn" }));
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
